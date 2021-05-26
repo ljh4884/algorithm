eec94a1d1cea3286ca7f4ee1be6917f962816185
Lee JeongHwan <bluestar724@naver.com>
2021-05-27 06:41:47 +0900

    arctic

--- algospot/2021/024_arctic/backup.cpp
+++ algospot/2021/024_arctic/backup.cpp
@@ -2,7 +2,6 @@
 #include <math.h>
 #include <algorithm>
 
-
 using namespace std;
 
 
@@ -14,56 +13,20 @@ typedef struct data
 	double value;
 }data;
 
-int check[100];
-
 
 data map[100];
 double dis[100][100];
-double list[5000];
 int n;
-int cnt;
 
+bool compare(const data& d1, const data& d2)
+{
+	return d1.value < d2.value;
+}
 
 
 double cal(int pos1,int pos2)
 {
 	return sqrt((map[pos1].x - map[pos2].x) * (map[pos1].x - map[pos2].x) + (map[pos1].y - map[pos2].y) * (map[pos1].y - map[pos2].y));
-}
-
-int func(double r)
-{
-
-	for(int i=1;i<n;i++)
-		check[i] = 0;
-	check[0] = 1;
-
-	while(1)
-	{
-		int cond = 1;
-		for(int i=0;i<n;i++)
-		{
-			if(check[i] == 1)
-			{
-				cond = 0;
-				check[i] = 2;
-				for(int j=0;j<n;j++)
-				{
-					if(check[j] == 0)
-						if(dis[i][j] <= r)
-							check[j] = 1;
-				}
-			}
-		}
-		if(cond)
-			break;
-		
-	}
-
-
-	for(int i=0;i<n;i++)
-		if(check[i] == 0)
-			return 0;
-	return 1;
 }
 
 
@@ -78,28 +41,45 @@ int main()
 		for(int i=0;i<n;i++)
 		{
 			scanf("%lf %lf",&map[i].x,&map[i].y);
+			map[i].index = i;
+			map[i].value = 2000;
 		}
-		cnt = 0;
+		
 		for(int i=0;i<n;i++)
 		{
 			for(int j=i+1;j<n;j++)
 			{
 				dis[i][j] = cal(i,j);
 				dis[j][i] = dis[i][j];
-				list[cnt] = dis[i][j];
-				cnt++;
 			}
 		}
 
-		sort(list,list+cnt);
-		
-		for(int i=0;i<cnt;i++)
+		for(int i=1;i<n;i++)
+			map[i].value = dis[0][i];
+		map[0].value = 0;
+		for(int i=1;i<n;i++)
 		{
-			if(func(list[i]))
+			sort(map+i,map+n,compare);
+
+
+			
+
+			for(int j=i+1;j<n;j++)
 			{
-				printf("%.2lf\n",list[i]);
-				break;
+				map[j].value = min(map[j].value,dis[map[i].index][map[j].index]);
+				map[j].value = max(map[j].value,map[i].value);
 			}
 		}
+		double temp;
+		for(int i=1;i<n;i++)
+			temp = max(temp,map[i].value);
+		printf("%.2lf\n",temp);
+		
+
+
+
+
+
+
 	}
 }
