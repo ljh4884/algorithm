5bf6aa3a3c7ef8e498c905ad22ad9216bbadf70b
Lee JeongHwan <bluestar724@naver.com>
2021-05-28 02:31:50 +0900

    s

--- algospot/2021/025_canadatrip/source.cpp
+++ algospot/2021/025_canadatrip/source.cpp
@@ -9,7 +9,6 @@ typedef struct data
 	int end;
 	int cnt;
 	int g;
-	int cur;
 }data;
 
 bool compare(const data& d1,const data& d2)
@@ -23,7 +22,24 @@ bool compare(const data& d1,const data& d2)
 data sign[5000];
 int check[5000];
 int n;
-long long k;
+int k;
+
+
+
+int func(int start, int end)
+{
+	int mid = (start+end)/2;
+	int cnt = 0;
+	for(int i=0;i<n;i++)
+	{
+		if(sign[i].end <= mid)
+			cnt += sign[i].cnt;
+		else if(sign[i].start <= mid)
+			cnt += (mid - sign[i].start) / sign[i].g + 1;
+	}
+	return cnt;
+}
+
 int main()
 {
 	int c;
@@ -31,7 +47,8 @@ int main()
 	while(c--)
 	{
 		int pos,m,g;
-		scanf("%d %lld",&n,&k);
+		int end = 0;
+		scanf("%d %d",&n,&k);
 
 		for(int i=0;i<n;i++)
 		{
@@ -41,88 +58,34 @@ int main()
 			sign[i].start = pos - m;
 			sign[i].cnt = m/g + 1;
 			sign[i].g = g;
-			sign[i].cur = sign[i].start;
 			check[i] = 0;
+			end = max(end,sign[i].end);
 		}
 
-		sort(sign,sign+n,compare);
-
-
-		long long cnt = 0;
-		int end_pos = 8030001;
-		int last_index = n;
-		int start_pos = 0;
-		for(int i=0;i<n;i++)
+		sort(sign,sign+n,compare);	
+		int start = -1;
+		while(1)
 		{
+			int cnt = func(start,end);
+			int mid = (start+end)/2;
 			
-			int sum = 0;
-			int temp_start = 8030001;
-			for(int j=0;j<i;j++)
+			if(start + 1 == end)
 			{
-				if(check[j] == 0)
-				{
-					if(sign[j].end < sign[i].start)
-					{
-						check[j] = 1;
-						temp_start = min(temp_start,sign[j].start);
-						sum+=sign[j].cnt;
-					}
-				}
-			}
-
-			if(cnt+sum > k)
-			{
-				start_pos = temp_start;
-				end_pos = sign[i].start;
-				last_index = i;
+				printf("%d\n",end);
 				break;
 			}
+
+			if(cnt < k)
+				start = mid;
+			else
+				end = mid;
 			
-			cnt+=sum;
-
-			for(int j=0;j<i;j++)
-			{
-				if(check[j] == 1)
-					check[j] = 2;
-			}
-		}
-
-		k-=cnt;
-		int cond = 0;
-		for(int pos = start_pos;pos<end_pos;pos++)
-		{
-			for(int i=0;i<n;i++)
-			{
-				if(k == 1)
-				{
-					cond = 1;
-					break;
-				}
-				if(check[i] < 2)
-				{
-					if(pos == sign[i].cur)
-					{
-						k--;
-						sign[i].cur+=sign[i].g;
-						if(sign[i].cur > sign[i].end)
-						{
-							check[i] = 2;
-						}
-
-					}
-				}
-			}
-			if(cond == 1)
-			{
-				printf("%d\n",pos);
-				break;
-			}
-		}
-
-
-
+			
 		
 
+		}
+		
+		
 
 	}
 
