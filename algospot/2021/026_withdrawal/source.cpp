6aa4a42a5838ff9314d4438bf479e34d9ca681b1
Lee JeongHwan <bluestar724@naver.com>
2021-05-28 17:36:50 +0900

    dd

--- algospot/2021/026_withdrawal/source.cpp
+++ algospot/2021/026_withdrawal/source.cpp
@@ -1,9 +1,47 @@
 #include <stdio.h>
+#include <algorithm>
 
+using namespace std;
+
+
+
+
+typedef struct data
+{
+	double rank;
+	double card;
+	double divi;
+}data;
+
+
+bool compare(const data& d1, const data& d2)
+{
+	if(d1.divi == d2.divi)
+		return d1.card > d2.card;
+	return d1.divi < d2.divi;
+}
 
 int n,k;
-int r[1000];
-int c[1000];
+data st[1000];
+int check[1000];
+
+
+double func(int count,double sum1,double sum2)
+{
+	if(count == n)
+		return score;
+	double temp = 1;
+	double temp_index = -1;
+	for(int i=0;i<n;i++)
+	{
+		if(check[i] == 1)
+			continue;
+
+
+		
+	}
+}
+
 int main()
 {
 	int t;
@@ -12,6 +50,23 @@ int main()
 	{
 		scanf("%d %d",&n,&k);
 		for(int i=0;i<n;i++)
-			scanf("%d %d",&r[i],&c[i]);
+		{
+			scanf("%lf %lf",&st[i].rank,&st[i].card);
+			st[i].divi = st[i].rank / st[i].card;
+		}
+	
+		sort(st,st+n,compare);
+
+		double sum1 = 0;
+		double sum2 = 0;
+		for(int i=0;i<k;i++)
+		{
+			sum1+=st[i].rank;
+			sum2+=st[i].card;
+		}
+
+
+		printf("%lf\n",sum1/sum2);
+
 	}
 }
